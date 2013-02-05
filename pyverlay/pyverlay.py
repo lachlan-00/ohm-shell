dd3a2d39f53aea139cadf42c3f5b8efec74bc2bf
lachlan-00 <lachlan.00@gmail.com>
2013-02-05 16:44:01 +1000

    more additions. changing pc

--- pyverlay/pyverlay.py
+++ pyverlay/pyverlay.py
@@ -29,6 +29,7 @@ import mimetypes
 import ConfigParser
 
 from gi.repository import Gtk
+from gi.repository import Gdk
 from xdg.BaseDirectory import xdg_config_dirs
 
 
@@ -49,6 +50,7 @@ class PYVERLAY(object):
         self.builder.connect_signals(self)
         # main window
         self.window = self.builder.get_object("main_window")
+        self.activities = self.builder.get_object("hot_corner")
         self.fileview = self.builder.get_object("fileview")
         self.contentlist = self.builder.get_object('filestore')
         self.contenttree = self.builder.get_object('fileview')
@@ -60,6 +62,9 @@ class PYVERLAY(object):
     def run(self, *args):
         """ Connect events and show the main window """
         self.window.connect("destroy", self.quit)
+        self.activities.connect("motion-notify-event", self.motion)
+        self.activities.connect("key-release-event", self.keycatch)
+        self.window.connect("key-release-event", self.keycatch)
         self.closebutton.connect("clicked", self.quit)
         # set up file and folder lists
         cell = Gtk.CellRendererText()
@@ -67,9 +72,29 @@ class PYVERLAY(object):
         self.fileview.connect("row-activated", self.loadselection)
         self.contenttree.append_column(filecolumn)
         self.contenttree.set_model(self.contentlist)
-        print self.loadselection()
+        print len(self.loadselection())
+        self.activities.show()
         self.window.show()
         Gtk.main()
+
+    def keycatch(self, actor, event):
+        test_mask = (event.state & Gdk.ModifierType.SUPER_MASK ==
+                       Gdk.ModifierType.SUPER_MASK)
+        #print dir(Gdk.ModifierType)
+        #print event.state & Gdk.ModifierType.SUPER_MASK
+        if event.get_state() and test_mask:
+            if self.window.get_visible():
+                self.hide()
+            elif self.window.get_visible():
+                self.show()
+
+    def motion(self, actor, event):
+        #print dir(event.state)
+        #print dir(Gdk.ModifierType)
+        test_mask = (event.state & Gdk.ModifierType.META_MASK ==
+                       Gdk.ModifierType.META_MASK)
+        print event.get_state()
+        return
 
     def show(self, *args):
         """ fill and show the config window """
@@ -100,7 +125,7 @@ class PYVERLAY(object):
             filelist.sort(key=lambda y: y.lower())
             for files in filelist:
                 if files[(files.rfind('.')):] == '.desktop':
-                    self.current_files.append(current_dir + '/' + files)
+                    self.current_files.append(items + '/' + files)
         #model, fileiter = self.contenttree.get_selection().get_selected_rows()
         #for files in fileiter:
         #    tmp_file = current_dir + '/' + model[files][0]
